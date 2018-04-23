03e451cc75fd271cf08d40f51aa92bf430a73e01
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to create a Sparkline control
+
+
+<p>This example demonstrates how to create a sparkline control, fill it with data, choose the way to represent this data, and customize sparkline appearance.</p>
+
+
+<h3>Description</h3>
+
+<p>To do this, you should create a SparklineEdit object and assign the datasource object to its SparklineEdit.EditValue property. </p><p>Then, you need to set the appropriate view instance to the SparklineEdit.Properties.View property and customize view properties to meet your requirements.</p>
+
+<br/>
+
+
---
+++ VB/SparklineSample/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#usings"
+Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.Sparkline
+Imports DevExpress.XtraEditors
+#End Region ' #usings
+
+Namespace SparklineSample
+
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			Me.Controls.Add(CreateSparkline())
+		End Sub
+
+		#Region "#Sparkline"
+		Private Function CreateSparkline() As SparklineEdit
+			' Create a Sparkline editor and set its dock style.
+			Dim sparkline As New SparklineEdit()
+			sparkline.Dock = DockStyle.Fill
+
+			sparkline.EditValue = CreateData()
+
+			' Create an Area view and assign it to the sparkline.
+			Dim view As New AreaSparklineView()
+			sparkline.Properties.View = view
+
+			' Customize area appearance.
+			view.Color = Color.Aqua
+			view.AreaOpacity = 50
+
+			' Show markerks.
+			view.HighlightStartPoint = True
+			view.HighlightEndPoint = True
+			view.HighlightMaxPoint = True
+			view.HighlightMinPoint = True
+			view.HighlightNegativePoints = True
+			view.SetSizeForAllMarkers(10)
+
+			Return sparkline
+		End Function
+
+		Private Function CreateData() As Double()
+			Return New Double() { 2, 4, 5, 1, -1, -2, -1, 2, 4, 5, 6, 3, 5, 4, 8, -1, 6 }
+		End Function
+		#End Region ' #Sparkline
+	End Class
+
+End Namespace
